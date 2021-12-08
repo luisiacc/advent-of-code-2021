2aeeea3ff0a7b40139449da54baf83a65a5c769d
luisiacc <lccpacc@gmail.com>
2021-12-07 23:46:29 -0500

    day 7

---
+++ day7/main.py
@@ -0,0 +1,22 @@
+#!usr/bin/env python3.9
+def get_input() -> str:
+    with open("input.txt", "r") as f:
+        return f.read()
+
+
+def main():
+    crabs = sorted(map(int, get_input().split(",")))
+
+    best = 10000000000000
+    for med in range(max(crabs)):
+        score = 0
+        for c in crabs:
+            score += abs(c - med)
+        if score < best:
+            best = score
+    print(best)
+
+
+if __name__ == "__main__":
+    main()
+
